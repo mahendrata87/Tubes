7a92ed039617da46b9e7a9927ada54d1ab0d449e
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 02:54:37 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -10,13 +10,19 @@ with open("kode_negara_lengkap.json") as f:
            data = json.load(f)
     
 dfjson = pd.DataFrame.from_dict(data)
-
+for country_name in data:
+           data
 #Plot1
-country = st.selectbox('Select Country:', 
-                       fordfjson['name'])
-kode = 'AUS'
+#country = st.selectbox('Select Country:', 
+#                       fordfjson['name'])
+
+#st.write('You selected:', country)
+
 df = pd.read_csv('produksi_minyak_mentah.csv')
-dff = df.loc[df['kode_negara'] == kode]
+dfc=df[df.tahun != 1974]
+dfc=df.drop(df.index[df['tahun']==1974])
+
+dff = dfc.loc[dfc['kode_negara'] == kode]
 print(dff)
 plt.plot(dff['tahun'], dff['produksi'])
 
@@ -28,7 +34,7 @@ st.pyplot(fig)
 #Plot2
 int_slide = st.slider('Tahun', min_value=1971, max_value=2015, value=1971, step=1)
 int_num = st.number_input('Berapa Besar', min_value=1, max_value=15, value=5, step=1)
-df2= df.loc[df['tahun'] == int_slide]
+df2= dfc.loc[dfc['tahun'] == int_slide]
 df3 = df2.sort_values(["produksi"], ascending=False)
 df4 = df3.head(int(int_num))
 df5 = df4.merge(dfjson, left_on=['kode_negara'], right_on=["alpha-3"], how='inner')
