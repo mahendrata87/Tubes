f7011d4e26530f9e135da4a1f6dcaec10d9b6450
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 01:04:38 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Analisis Data Produksi Minyak Dunia")
+
+st.sidebar.title("Pengaturan")
+left_col, mid_col, right_col = st.columns(3)
+
+st.sidebar.subheader("Pengaturan konfigurasi tampilan")
