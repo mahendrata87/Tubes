47001a35a397edbc358436e068ddbcc5ea0096e0
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 01:05:28 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -5,5 +5,3 @@ st.title("Analisis Data Produksi Minyak Dunia")
 
 st.sidebar.title("Pengaturan")
 left_col, mid_col, right_col = st.columns(3)
-
-st.sidebar.subheader("Pengaturan konfigurasi tampilan")
