8308e12eea411fa2bfd35d49475e79731c77d593
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 01:08:12 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import json
 
 st.set_page_config(layout="wide")
 st.title("Analisis Data Produksi Minyak Dunia")
 
 st.sidebar.title("Pengaturan")
 left_col, mid_col, right_col = st.columns(3)
+
+kode = input('kode negara = ')
+df = pd.read_csv('produksi_minyak_mentah.csv')
+dff = df.loc[df['kode_negara'] == kode]
+#print(dff)
+plt.plot(dff['tahun'], dff['produksi'])
+plt.show()
