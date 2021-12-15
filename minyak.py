c1d9390585a6447cd66d636a89e1b772d15b495e
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 01:27:04 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -14,4 +14,8 @@ df = pd.read_csv('produksi_minyak_mentah.csv')
 dff = df.loc[df['kode_negara'] == kode]
 #print(dff)
 plt.plot(dff['tahun'], dff['produksi'])
-st.write(plt.show())
+
+fig, ax = plt.subplots()
+ax.plot(dff['tahun'], dff['produksi'])
+
+st.pyplot(fig)
