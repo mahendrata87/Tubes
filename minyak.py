c557c8e75c5a44f07e27e32b0c0c26e0ead11d5b
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 17:37:37 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -39,7 +39,7 @@ negara = select
 dff = df_baru.loc[df_baru['kode_negara'] == negara]
 plt.plot(dff['tahun'], dff['produksi'])
 
-fig, ax = plt.subplots(gridspec_kw=1)
+fig, ax = plt.subplots(nrows=1, ncols=1, figure=self, gridspec_kw)
 ax.plot(dff['tahun'], dff['produksi'])
 
 st.pyplot(fig)
