bf15030520e5f53c549cc5af8abd6f14e02ad237
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 23:46:13 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide")
 st.title("Aplikasi Analisis Data Produksi Minyak Mentah Dunia")
 st.sidebar.title("Pengaturan")
 
-image = Image.open('oil_rig.png')
+image = Image.open('oil.jpg')
 st.sidebar.image(image)
 
 df = pd.read_csv('produksi_minyak_mentah.csv')
