7ef1b67d1adf28d3dac5457d5ec7016f18ac9a42
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 23:02:15 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -62,6 +62,7 @@ df4 = df3.head(int(int_num))
 
 fig2, axs = plt.subplots()
 axs.bar(df4['kode_negara'], df4['produksi'])
+axs.xticks(rotation=80)
 st.pyplot(fig2)
 
 #==================================================== Plot3 ====================================================
