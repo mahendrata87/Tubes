16c4750f03c6c549e1854c178d366b14d8a8ef61
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-16 23:39:37 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
-import pillow
+import PIL
 
 st.set_page_config(layout="wide")
 st.title("Aplikasi Analisis Data Produksi Minyak Mentah Dunia")
