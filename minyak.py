fe3af8bd0cee786bcd30d75ebdb28b546c3295d8
Syahrial Fitrachman <89689063+mahendrata87@users.noreply.github.com>
2021-12-17 09:15:56 +0700

    Update minyak.py

--- minyak.py
+++ minyak.py
@@ -9,7 +9,7 @@ st.set_page_config(layout="wide")
 st.title("Aplikasi Analisis Data Produksi Minyak Mentah Dunia")
 st.subheader('dibuat oleh Syahrial Fitrachman (12220060)')
 
-st.sidebar.title('Selamat Datang!')
+st.sidebar.title('Selamat Datang !')
 image = Image.open('oils.png')
 st.sidebar.image(image)
 
